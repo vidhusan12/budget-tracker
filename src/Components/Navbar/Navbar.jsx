a49a1c111a965ea8fb189752d2e377e208a33694
vidhusan12 <vidhusan709@gmail.com>
2025-10-18 18:19:28 +1300

    feat: add mobile responsive design with hamburger menu for navbar and optimized layouts

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Function to toggle the menu
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen)
+  }
   return (
     <nav className='navbar'>
       <div className="navbar-left">
         <h2>Budget Tracker</h2>
+        <button className='hamburger' onClick={toggleMenu}>
+          {isMenuOpen ? 'x' : '☰'}
+        </button>
       </div>
       <div className="navbar-right">
-        <ul className='nav-links'>
+        <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
           <li><Link to="/"> DashBoard</Link></li>
           <li><Link to="/add-expense"> Add Transcation</Link></li>
           <li><Link to="/add-income">Add Income</Link></li>
